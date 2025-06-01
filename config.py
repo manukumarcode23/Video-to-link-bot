6c4a257b34dfee0759075f4cb6e06cf504dfd90b
manukumarcode23 <onetaplooters@gmail.com>
2025-06-01 14:18:26 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+# Don't Remove Credit @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+# Temporary storage
+class temp(object):
+    ME = None
+    BOT = None
+    U_NAME = None
+    B_NAME = None
+
+# Exceptions
+class InvalidHash(Exception):
+    message = "Invalid hash"
+
+class FIleNotFound(Exception):
+    message = "File not found"
